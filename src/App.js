0395c3fc9c75886c078ca416ad8b1c749df24b8c
yumenglan <yumenglan@maopp.cn>
2018-10-08 16:06:18 +0800

    init program

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This is React App
+      </div>
+    );
+  }
+}
+
+export default App;
